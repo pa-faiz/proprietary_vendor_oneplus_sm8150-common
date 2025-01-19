3f8db25e1fa59a1ac37916f48b89499026c5c2a4
Fazil Sheik <fazil.sheik96@gmail.com>
2025-01-19 17:55:15 +0530

    Revert "sm8150-common: Drop libril-qcril-hook-oem.so"
    This reverts commit 1ccf8937f5957eee9a982007a9489fddae8c54f4.
    
    Change-Id: I1f01c4b7688876677e9ad42f6482961b2ee95a7e

--- sm8150-common-vendor.mk
+++ sm8150-common-vendor.mk
@@ -494,6 +494,7 @@ PRODUCT_PACKAGES += \
     libril-qc-logger \
     libril-qc-ltedirectdisc \
     libril-qc-radioconfig \
+    libril-qcril-hook-oem \
     librilqmiservices \
     librpmb \
     libsecureui \
